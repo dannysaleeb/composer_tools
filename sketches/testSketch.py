1f7b8138e011bf2a91f65d953d309b4c66854bf4
Danny Saleeb <dannysaleeb@gmail.com>
2022-11-22 22:07:16 +0000

    nothing changed, just fiddling

--- sketches/testSketch.py
+++ sketches/testSketch.py
@@ -6,5 +6,5 @@ from playback.players import serialPlayer, parallelPlayer
 
 myScale = scales.Scale([2,1,2,2,2,1,2], notes.NOTES['Eb'], -1)
 
-serialPlayer([sequence.Sequence([4, 2], ["down", "up"]).getSequenceRootsMIDI(myScale, 1, 3)], 120)
+serialPlayer([sequence.Sequence([4, 2], ["down", "up"]).getSequenceRootsMIDI(myScale, 1, 3)], 240)
 

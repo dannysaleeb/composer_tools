95ea1d321625e7489173d91ab6ec9f4e3be468de
Danny Saleeb <dannysaleeb@gmail.com>
2022-12-03 23:35:51 +0000

    generating xml without <part-list> so far

--- classes/score_again.py
+++ classes/score_again.py
@@ -57,24 +57,75 @@ class Node:
             for child in root.children:
                 res = res + self.preorder_traversal(child)
         return res
+    # for some reason this is a bit of a mess... 
+    """
 
+    Need to get the Node content if it's a dict, and then whack it in at the right tab length ... (which needs to be applied
+    to each line within that content, so I guess it actually needs to go into the function also ... and add to the other amounts)
+    
+    The only problem is, "content" is being rendered as being between two tags on same line -- when it's a dict
+    the tags need to be treated like other tags...
+
+    """
     def get_xml(self, root):
         xml = []
         if root:
             # first check if it's the leaf node (if leaf?) and return fully closed element if so
             if len(root.children) < 1:
                 content = root.data['xml']['content']
-                for k,v in root.data['xml']['element'].items():
-                    xml.append("\t" * root.depth + f"<{k}{v}>{content}</{k}>")
+                # This needs to be treated in the same way as the open tags ... tricky, because this is being treated as a leaf currently
+                if isinstance(content, dict):
+                    content = self.get_xml_content(root.data["xml"]["content"], root.depth)
+                    for k,v in root.data["xml"]["element"].items():
+                        # Why is root.depth always 0 here?
+                        print(root.depth)
+                        xml.append("\t" * root.depth + f"<{k}{v}>\n{content}")
+                        xml.append("\t" * root.depth + f"</{k}>")
+                else:
+                    for k,v in root.data['xml']['element'].items():
+                        xml.append("\t" * root.depth + f"<{k}{v}>{content}</{k}>")
             # else do open tags which are closed after the recursion (not sure why k is accessible after the for loops though)
             else:
                 content = root.data["xml"]["content"]
+                # Why is this type str???
+                if isinstance(content, dict):
+                    content = self.get_xml_content(root.data["xml"]["content"], root.depth)
+                print(f"CONTENT 2 is: \n{content}")
                 for k,v in root.data["xml"]["element"].items():
                     xml.append("\t" * root.depth + f"<{k}{v}>{content}")
                 for child in root.children:
                     xml = xml + self.get_xml(child)
                 xml.append("\t" * root.depth + f"</{k}>")
         return xml
+
+    def build_xml_content(self, content_dict, tabs, counter=0):
+        # The return string
+        content = []
+        # for key, value in dictionary
+        for k,v in content_dict.items():
+            if not isinstance(v, dict):
+                content.append("\t" * (tabs + 1 + counter) + f"<{k}>{v}</{k}>\n")
+            else:
+                content.append("\t" * (tabs + 1 + counter) + f"<{k}>\n")
+                counter += 1
+                content = content + self.build_xml_content(v, tabs, counter)
+                counter -= 1
+                content.append("\t" * (tabs + 1 + counter) + f"</{k}>\n")
+
+        return content
+
+    def get_xml_content(self, content_dict, tabs, counter=0):
+
+
+        content_string = ''
+        
+        content_list = self.build_xml_content(content_dict, tabs, counter)
+        # strip last newline from content
+        content_list[-1] = content_list[-1][:-1]
+        for item in content_list:
+            content_string += item
+
+        return content_string
 
 class Score(Node):
     def __init__(self, type="score-partwise", parent=None):
@@ -122,9 +173,29 @@ class Part(Node):
                 },
                 "midi": {}
         }
+
+        for child in self.parent.children:
+            if type(child) is Partlist:
+                child.data["xml"]["content"][""]
     
     def add_measure(self, number):
         self.measures.append(Measure(number, parent=self))
+
+# This is also problematic!!! but I will overcome it.
+class Partlist(Node):
+    def __init__(self, type="part-list", parent=None):
+        super().__init__(type, parent)
+
+        self.data = {
+                "xml": {
+                    "empty": False,
+                    "element": {
+                        f"{self.node_type}": ""
+                    },
+                    "content": ""
+                },
+                "midi": {}
+        }
 
 class Meta(Node):
     def __init__(self, type, parent=None):
@@ -205,18 +276,37 @@ class Note(Node):
                 },
                 "midi": {}
         }
-    # NEED TO FIX
-    def get_xml_content(self, element, counter=0):
-        content = ""
-        for k,v in element.items():
-            if isinstance(v, dict):
-                content += "\t" * counter + f"<{k}>"
-                counter += 1
-                self.get_xml_content(v)
+    # NEED TO FIX -- if it's the last in the process, don't add a space -- maybe do this by adding to a list?
+    def build_xml_content(self, content_dict, counter=0):
+        # The return string
+        content = []
+        # for key, value in dictionary
+        for k,v in content_dict.items():
+            if not isinstance(v, dict):
+                content.append("\t" * counter + f"<{k}>{v}</{k}>\n")
             else:
-                content += "\t" * counter + f"<{k}>{v}</{k}>"
-            content += "\t" * counter + f"</{k}>"
+                content.append("\t" * counter + f"<{k}>\n")
+                counter += 1
+                content = content + self.build_xml_content(v, counter)
+                counter -= 1
+                content.append("\t" * counter + f"</{k}>\n")
+
+        # strip last newline from content
+        content[-1] = content[-1][:-1]
+
         return content
+
+    def get_xml_content(self, content_dict, counter=0):
+        content_string = ''
+        
+        content_list = self.build_xml_content(content_dict, counter)
+        for item in content_list:
+            content_string += item
+
+        return content_string
+
+
+    
 
 if __name__ == "__main__":
 
@@ -233,17 +323,17 @@ if __name__ == "__main__":
             for i in range(10):
                 child.add_child(Measure(i+1, "measure", child))
 
+    for child in score.children:
+        for grandchild in child.children:
+            grandchild.add_child(Note("C", 4, 1, parent=grandchild))
+            for note in grandchild.children:
+                print(note.depth)
+
+# Need to find a way of making sure each node gets depth ... when they're attached to the node above ... (or attached
+# using add_child() method)
 
     for item in score.get_xml(score):
         print(item)
-
-    note = Note("C", 4, 1)
-
-    content = note.get_xml_content(note.data["xml"]["content"])
-
-    print(content)
-
-
 
 """
 
